e530979fc7d546c16550b9ac79a358d418674e64
Tiago Tamagusko <tamagusko@gmail.com>
2022-03-19 00:01:44 +0000

    add more images to train model

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+st.write("""
+         # Rock-Paper-Scissor Hand Sign Prediction
+         """
+         )st.write("This is a simple image classification web app to predict rock-paper-scissor hand sign")file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
